16ca779f7cb87ad2692195e9c4fd779be49133e8
Tal Weiss <weisstal@gmail.com>
2023-02-27 21:39:37 +0200

    additions

--- DBeaver_Pass_Decryptor and profiler.py
+++ DBeaver_Pass_Decryptor and profiler.py
@@ -7,6 +7,7 @@ import base64
 import os
 import json
 import platform
+from ast import literal_eval
 
 # pip install pycryptodome
 
@@ -41,11 +42,53 @@ print(filepath)
 # PASSWORD_DECRYPTION_KEY = bytes([-70, -69, 74, -97, 119, 74, -72, 83, -55, 108, 45, 101, 61, -2, 84, 74])
 PASSWORD_DECRYPTION_KEY = bytes([186, 187, 74, 159, 119, 74, 184, 83, 201, 108, 45, 101, 61, 254, 84, 74])
 
+# opening the object as a bytes object (Raw Binary), not a str object
 data = open(filepath, 'rb').read()
 
 decryptor = AES.new(PASSWORD_DECRYPTION_KEY, AES.MODE_CBC, data[:16])
 padded_output = decryptor.decrypt(data[16:])
 output = padded_output.rstrip(padded_output[-1:])
+print(type(output))
+output_to_json = literal_eval(output.decode('utf8'))
+print(type(output_to_json))
+
+
+# JSON data that we want to add to a new data-sources 2.json file in the \.dbeaver folder
+def create_new_data_sources_file(name=None, host=None, database=None):
+    data_sources = {
+        "folders": {},
+        "connections": {
+            "postgres-jdbc-1840428109c-5738813cb6c37d2b": {
+                "provider": "postgresql",
+                "driver": "postgres-jdbc",
+                "name": name,
+                "save-password": true,
+                "read-only": false,
+                "configuration": {
+                    "host": host,
+                    "port": "5432",
+                    "database": database,
+                    "url": "jdbc:postgresql://" + host + ":5432/" + database + "\"",
+                    "type": "dev",
+                    "provider-properties": {
+                        "@dbeaver-show-non-default-db@": "false",
+                        "@dbeaver-show-template-db@": "false",
+                        "@dbeaver-show-unavailable-db@": "false",
+                        "show-database-statistics": "false",
+                        "@dbeaver-read-all-data-types-db@": "false",
+                        "read-keys-with-columns": "false",
+                        "@dbeaver-use-prepared-statements-db@": "false",
+                        "postgresql.dd.plain.string": "false",
+                        "postgresql.dd.tag.string": "false"
+                    },
+                    "auth-model": "native"
+                }
+            }
+        }
+    }
+    with open(default_paths / "data-sources - 2.json", "w") as outfile:
+        json.dump(data_sources, outfile)
+
 
 try:
     print(json.dumps(json.loads(output), indent=4, sort_keys=True))
